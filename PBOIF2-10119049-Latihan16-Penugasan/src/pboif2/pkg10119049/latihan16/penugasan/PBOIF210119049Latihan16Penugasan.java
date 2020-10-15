e4bced6d95abfd64db1afcd3d2a97fcc5faafe3d
Nur Sasongko <32429721+ssasongko@users.noreply.github.com>
2020-10-15 21:48:33 +0700

    Add files via upload

---
+++ PBOIF2-10119049-Latihan16-Penugasan/src/pboif2/pkg10119049/latihan16/penugasan/PBOIF210119049Latihan16Penugasan.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119049.latihan16.penugasan;
+
+/**
+ * @author 
+ * Nama  : Nur Sasongko
+ * Kelas : PBOIF2
+ * NIM   : 10119049
+ * Deskripsi Program : Program penugasan
+ */
+public class PBOIF210119049Latihan16Penugasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here 
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c + "\n");
+        
+        c = a + b;
+        System.out.println("c = a + b = " + c );
+        
+        c += a;
+        System.out.println("c = a + b = " + c );
+        
+        c -= a;
+        System.out.println("c = a + b = " + c );
+        
+        c *= a;
+        System.out.println("c *= a = " + c );
+        
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a = " + c);
+        
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c);
+        
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        
+        c >>= 2;
+        System.out.println("c >>= a = " + c);
+        
+        c &= a;
+        System.out.println("c &= 2 = " + c);
+        
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        
+        c |= a;
+        System.out.println("c |= a = " + c);
+ 
+    } 
+    
+}
